ae7b2e9966d2a53766695eeddf1f1995ccf54e6d
Kaothara Morakinyo <kmorakinyo2021@gmail.com>
2023-07-12 18:26:16 +0000

    first commit

---
+++ forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    select = SelectField(u'Doctor or Patient', choices=[('dt', 'Doctor'), ('pt', 'Patient')])
+    submit = SubmitField('Sign In')
---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
+from flask_behind_proxy import FlaskBehindProxy
+
+app = Flask(__name__)
+proxied = FlaskBehindProxy(app) 
+app.config['SECRET_KEY'] = 'f46d9cffd6774fefbe29c89f0ed3dc39'
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', subtitle='', text='Please Select Login Portal Above.')
+
+# @app.route("/doctor_login")
+# def doct():
+#     return render_template('doctor.html', subtitle='Doctor Sign In Page', text='Please Log In.')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Log In', form=form)
+
+
+# @app.route("/patient_login", methods=['GET', 'POST'])
+# def register():
+#     form = RegistrationForm()
+#     if form.validate_on_submit(): # checks if entries are valid
+#         flash(f'Account created for {form.username.data}!', 'success')
+#         return redirect(url_for('home')) # if so - send to home page
+#     return render_template('register.html', title='Log In', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
