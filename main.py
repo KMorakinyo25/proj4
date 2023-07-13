3ff468acc634f628392feec1c9f229fc0e123d63
Kaothara Morakinyo <kmorakinyo2021@gmail.com>
2023-07-13 23:50:53 +0000

    latest by Kao

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class User(db.Model):
   username = db.Column(db.String(20), unique=True, nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
   password = db.Column(db.String(60), nullable=False)
-  #select = db.Column(db.String())
+  select = db.Column(db.String(2), nullable= False)
 
 
   def __repr__(self):
@@ -29,30 +29,33 @@ with app.app_context():
 def home():
     return render_template('home.html', subtitle='', text='Please Select Login Portal Above.')
 
-<<<<<<< HEAD
+
 @app.route("/patient")
 def pat():
     return render_template('patient.html', subtitle='Patient Page', text='Welcome')
-=======
-@app.route('/recommend')
-def recommend():
-    return render_template('recommend.html')
 
-# @app.route("/doctor_login")
-# def doct():
-#     return render_template('doctor.html', subtitle='Doctor Sign In Page', text='Please Log In.')
->>>>>>> refs/remotes/origin/main
+@app.route("/symptoms")
+def symp():
+    return render_template('symptoms.html', subtitle='Symptoms', text='Please List Your Symptoms')
+
+@app.route("/doctor")
+def doct():
+    return render_template('doctors.html', subtitle='Doctor Page', text='Welcome')
+
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
-        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data, select=form.select.data)
         db.session.add(user)
         db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('pat'))
+        if user.select == 'pt':
+          return redirect(url_for('pat'))
+        else:
+          return redirect(url_for('doct'))
     return render_template('register.html', title='Log In', form=form)
 
 if __name__ == '__main__':
